ef06076f02f05908f450fd43485622677eb8b5e4
tanuj <tanuj.chilwal@ril.com>
2023-11-13 12:27:11 +0530

    using Carbon instead of time

--- helper/time.go
+++ helper/time.go
@@ -1,21 +1,13 @@
 package helper
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/golang-module/carbon/v2"
 )
 
 func GetUnixTimeInMs() int64 {
-
-	fmt.Println(carbon.Now().ToTimestampMilliStruct())
-	loc, _ := time.LoadLocation("Asia/Kolkata")
-
-	return time.Now().In(loc).UnixMilli()
+	return carbon.Now("Asia/Kolkata").TimestampMilli()
 }
 
 func GetUnixTimeInNanoSecond() int64 {
-	loc, _ := time.LoadLocation("Asia/Kolkata")
-	return time.Now().In(loc).UnixNano()
+	return carbon.Now("Asia/Kolkata").TimestampNano()
 }
